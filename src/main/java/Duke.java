c6e13810580822cdd7037ee9827bcf8e848cff5a
raniceyue <9975316+raniceyue@users.noreply.github.com>
2020-01-24 18:07:32 +0800

    Add Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
@@ -10,6 +11,10 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n\n";
         System.out.println("Greetings mortal, my name is\n" + logo + "How may I help you?\n");
+
+        String[] list = new String[101];
+        int numItems = 0;
+
         Scanner sc = new Scanner(System.in);
         while (sc.hasNextLine()) {
             String input = sc.nextLine();
@@ -17,8 +22,17 @@ public class Duke {
                 String output = border + "\n" + "\tSee u the next time you test ur code :P\n\n" + border;
                 System.out.println(output);
                 break;
+            } else if (input.equals("list")) {
+                String output = border + "\n\tHere is your list:\n";
+                for (int i = 1; list[i] != null; i++) {
+                    output += "\t(" + i + ") " + list[i] + "\n";
+                }
+                output += "\n" + border;
+                System.out.println(output);
             } else {
-                String output = border + "\n\t" + input + "\n\n" + border;
+                list[numItems + 1] = input;
+                numItems++;
+                String output = border + "\n\t ADDED: " + input + "\n\n" + border;
                 System.out.println(output);
             }
         }
