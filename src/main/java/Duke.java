94ddb51bb7b6e25149ff799eb38bb276e5bf8492
raniceyue <9975316+raniceyue@users.noreply.github.com>
2020-01-24 17:46:11 +0800

    Add Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,26 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+
+        String border = "=^..^=   =^..^=   =^..^=    =^..^=    =^..^=    =^..^=    =^..^=\n";
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+                + "|____/ \\__,_|_|\\_\\___|\n\n";
+        System.out.println("Greetings mortal, my name is\n" + logo + "How may I help you?\n");
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNextLine()) {
+            String input = sc.nextLine();
+            if (input.equals("bye")) {
+                String output = border + "\n" + "\tSee u the next time you test ur code :P\n\n" + border;
+                System.out.println(output);
+                break;
+            } else {
+                String output = border + "\n\t" + input + "\n\n" + border;
+                System.out.println(output);
+            }
+        }
     }
 }
